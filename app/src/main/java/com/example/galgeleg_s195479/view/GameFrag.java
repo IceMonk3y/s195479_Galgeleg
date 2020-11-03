b0b6065c08fc9ca3d56abce6aed4270c542a86cf
SImon <fridolf.lindhard@gmail.com>
2020-11-03 15:16:18 +0100

    Fix af gameFrag - Funktionelt spil nu (med net-dr adgang)

--- app/src/main/java/com/example/galgeleg_s195479/view/GameFrag.java
+++ app/src/main/java/com/example/galgeleg_s195479/view/GameFrag.java
@@ -52,202 +52,201 @@ public class GameFrag extends Fragment {
         ImageView imgV =(ImageView) v.findViewById(R.id.imageViewHang);
         switch (gl.getAntalForkerteBogstaver() ){
             case 0:
-                imgV.setImageResource(R.mipmap.ic_launcher);
+                imgV.setImageResource(R.drawable.galge);
                 break;
             case 1:
-                imgV.setImageResource(R.mipmap.ic_launcher);
+                imgV.setImageResource(R.drawable.forkert1);
                 break;
             case 2:
-                imgV.setImageResource(R.mipmap.ic_launcher);
+                imgV.setImageResource(R.drawable.forkert2);
                 break;
             case 3:
-                imgV.setImageResource(R.mipmap.ic_launcher);
+                imgV.setImageResource(R.drawable.forkert3);
                 break;
             case 4:
-                imgV.setImageResource(R.mipmap.ic_launcher);
+                imgV.setImageResource(R.drawable.forkert4);
                 break;
             case 5:
-                imgV.setImageResource(R.mipmap.ic_launcher);
+                imgV.setImageResource(R.drawable.forkert5);
                 break;
             case 6:
-                imgV.setImageResource(R.mipmap.ic_launcher);
+                imgV.setImageResource(R.drawable.forkert6);
                 break;
         }
     }
     private void listenButton(View v) {
-        Button a = v.findViewById(R.id.buttonA);
+        a = v.findViewById(R.id.buttonA);
         a.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v2) { checkLetter(v,"a"); }
         });
-        Button b = v.findViewById(R.id.buttonB);
+        b = v.findViewById(R.id.buttonB);
         b.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v2) {
                 checkLetter(v,"b");
             }
         });
-        Button c = v.findViewById(R.id.buttonC);
+        c = v.findViewById(R.id.buttonC);
         c.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v2) {
                 checkLetter(v,"c");
             }
         });
-        Button d = v.findViewById(R.id.buttonD);
+        d = v.findViewById(R.id.buttonD);
         d.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v2) {
                 checkLetter(v,"d");
             }
         });
-        Button e = v.findViewById(R.id.buttonE);
+        e = v.findViewById(R.id.buttonE);
         e.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v2) {
                 checkLetter(v,"e");
             }
         });
-        Button f = v.findViewById(R.id.buttonF);
+        f = v.findViewById(R.id.buttonF);
         f.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v2) {
                 checkLetter(v,"f");
             }
         });
-        Button g = v.findViewById(R.id.buttonG);
+        g = v.findViewById(R.id.buttonG);
         g.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v2) {
                 checkLetter(v,"g");
             }
         });
-        Button h = v.findViewById(R.id.buttonH);
+        h = v.findViewById(R.id.buttonH);
         h.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v2) {
                 checkLetter(v,"h");
             }
         });
-        Button i = v.findViewById(R.id.buttonI);
+        i = v.findViewById(R.id.buttonI);
         i.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v2) {
                 checkLetter(v,"i");
             }
         });
-        Button j = v.findViewById(R.id.buttonJ);
+        j = v.findViewById(R.id.buttonJ);
         j.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v2) {
                 checkLetter(v,"j");
             }
         });
-        Button k = v.findViewById(R.id.buttonK);
+        k = v.findViewById(R.id.buttonK);
         k.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v2) {
                 checkLetter(v,"k");
             }
         });
-        Button l = v.findViewById(R.id.buttonL);
+        l = v.findViewById(R.id.buttonL);
         l.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v2) {
                 checkLetter(v,"l");
             }
         });
-        Button m = v.findViewById(R.id.buttonM);
+        m = v.findViewById(R.id.buttonM);
         m.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v2) {
                 checkLetter(v,"m");
             }
         });
-        Button n = v.findViewById(R.id.buttonN);
+        n = v.findViewById(R.id.buttonN);
         n.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v2) {
                 checkLetter(v,"n");
             }
         });
-        Button o = v.findViewById(R.id.buttonO);
+        o = v.findViewById(R.id.buttonO);
         o.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v2) {
                 checkLetter(v,"o");
             }
         });
-        Button p = v.findViewById(R.id.buttonP);
+        p = v.findViewById(R.id.buttonP);
         p.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v2) {
                 checkLetter(v,"p");
             }
         });
-        Button q = v.findViewById(R.id.buttonQ);
+        q = v.findViewById(R.id.buttonQ);
         q.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v2) {
                 checkLetter(v,"q");
             }
         });
-        Button r = v.findViewById(R.id.buttonR);
+        r = v.findViewById(R.id.buttonR);
         r.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v2) {
                 checkLetter(v,"r");
             }
         });
-        Button s = v.findViewById(R.id.buttonS);
+        s = v.findViewById(R.id.buttonS);
         s.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v2) {
                 checkLetter(v,"s");
             }
         });
-        Button t = v.findViewById(R.id.buttonT);
+        t = v.findViewById(R.id.buttonT);
         t.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v2) {
                 checkLetter(v,"t");
             }
         });
-        Button u = v.findViewById(R.id.buttonU);
+        u = v.findViewById(R.id.buttonU);
         u.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v2) {
                 checkLetter(v,"u");
             }
         });
-        Button v1 = v.findViewById(R.id.buttonV);
+        v1 = v.findViewById(R.id.buttonV);
         v1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v2) {
                 checkLetter(v,"v");
             }
         });
-        Button w = v.findViewById(R.id.buttonW);
+        w = v.findViewById(R.id.buttonW);
         w.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v2) {
                 checkLetter(v,"w");
             }
         });
-        Button x = v.findViewById(R.id.butttonX);
+        x = v.findViewById(R.id.butttonX);
         x.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v2) {
                 checkLetter(v,"x");
             }
         });
-        Button y = v.findViewById(R.id.buttonY);
+        y = v.findViewById(R.id.buttonY);
         y.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v2) {
                 checkLetter(v,"y");
             }
         });
-        Button z = v.findViewById(R.id.buttonZ);
+        z = v.findViewById(R.id.buttonZ);
         z.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v2) {
                 checkLetter(v,"z");
             }
         });
-        Button æ = v.findViewById(R.id.buttonÆ);
+        æ = v.findViewById(R.id.buttonÆ);
         æ.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v2) {
                 checkLetter(v,"æ");
             }
         });
-        Button ø = v.findViewById(R.id.buttonØ);
+        ø = v.findViewById(R.id.buttonØ);
         ø.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v2) {
                 checkLetter(v,"ø");
             }
         });
-        Button å = v.findViewById(R.id.buttonÅ);
+        å = v.findViewById(R.id.buttonÅ);
         å.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v2) {
                 checkLetter(v,"å");
             }
         });
-
     }
 
 
