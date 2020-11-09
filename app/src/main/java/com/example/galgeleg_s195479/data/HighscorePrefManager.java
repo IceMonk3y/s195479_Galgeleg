2ca1ee9813dd81a8669d8b161f0bb21f432a327c
SImon <fridolf.lindhard@gmail.com>
2020-11-09 23:18:37 +0100

    Ufærdig gjort arbejde med historik/ highscore + constraint ændringer i main_menu.xml. (+ farve skift på text)

--- app/src/main/java/com/example/galgeleg_s195479/data/HighscorePrefManager.java
+++ app/src/main/java/com/example/galgeleg_s195479/data/HighscorePrefManager.java
@@ -3,6 +3,7 @@ package com.example.galgeleg_s195479.data;
 import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,22 +20,28 @@ public class HighscorePrefManager implements IHighscorePref {
     public void saveScore(Highscore highscore) {
         SharedPreferences share = act.getPreferences(Context.MODE_PRIVATE);
         SharedPreferences.Editor edit = share.edit();
+        //edit.putString("name",highscore.getName());
+       // edit.putInt("score",highscore.getScore());
         edit.putInt(highscore.getName(), highscore.getScore());
+
         System.out.println(highscore.getName());
         System.out.println(highscore.getScore());
     }
 
-
     @Override
     public List<Highscore> getScore() {
+
         SharedPreferences sharedPref = act.getPreferences(Context.MODE_PRIVATE);
         List<Highscore> highscoreList = new ArrayList<>();
 
-        Map<String, ?> key = sharedPref.getAll();
-        for (Map.Entry<String, ?> entry : key.entrySet()) {
-            Highscore newHigh = new Highscore(Integer.parseInt(entry.getValue().toString()), entry.getKey());
-            highscoreList.add(newHigh);
+        Map<String, ?> mapKey = sharedPref.getAll();
+        for (Map.Entry<String,?> entry : mapKey.entrySet()) {
+
+            Highscore list = new Highscore(Integer.parseInt(entry.getValue().toString()), entry.getKey());
+            highscoreList.add(list);
         }
+
+        Log.d("List ", String.valueOf(highscoreList));
         return highscoreList;
     }
 }
--- app/src/main/java/com/example/galgeleg_s195479/view/HighscoreFrag.java
+++ app/src/main/java/com/example/galgeleg_s195479/view/HighscoreFrag.java
@@ -1,145 +1,51 @@
 package com.example.galgeleg_s195479.view;
-
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.SharedPreferences;
+import android.app.Person;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AbsListView;
-import android.widget.Adapter;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
 
 import com.example.galgeleg_s195479.R;
 import com.example.galgeleg_s195479.data.Highscore;
 import com.example.galgeleg_s195479.data.HighscorePrefManager;
 import com.example.galgeleg_s195479.data.IHighscorePref;
-import com.example.galgeleg_s195479.logik.Galgelogik;
+import com.example.galgeleg_s195479.view.HighscoreAdapter;
 
-import java.util.List;
-import java.util.Objects;
-
+import java.util.ArrayList;
+// todo : Kan stadig ikke få highscore til at fungere. (Roddet forsøg basis) --> Baseret på https://www.youtube.com/watch?v=E6vE8fqQPTE&t=372s&ab_channel=CodingWithMitch
 public class HighscoreFrag extends Fragment {
-    private Galgelogik gl = Galgelogik.getInstance();
     private IHighscorePref IHighscorePref;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView recyclerView;
-    private RecyclerView.LayoutManager layoutManager;
 
-
-    SharedPreferences sp;
-    String[] highscoreArr;
-    ListView listView;
-
+    private  static final String TAG  = "MaiAct";
 
     @RequiresApi(api = Build.VERSION_CODES.N)
-    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        IHighscorePref = new HighscorePrefManager(getActivity());
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.highscore, container, false);
-        recyclerView = root.findViewById(R.id.listView);
+        Log.d(TAG, "onCreate: Started. ");
 
-        // use this setting to improve performance if you know that changes
-        // in content do not change the layout size of the RecyclerView
-        recyclerView.setHasFixedSize(true);
-        // use a linear layout manager
-        layoutManager = new LinearLayoutManager(this.getContext());
-        recyclerView.setLayoutManager(layoutManager);
+        IHighscorePref = new HighscorePrefManager(getActivity());
 
 
-        List<Highscore> highScores = IHighscorePref.getScore();
-        String[] highScoresStringArray = highScores.stream()
-                .map(x -> String.format("%s\t\t%s", x.getName(), x.getScore()))
-                .toArray(String[]::new);
+        ListView myListView = root.findViewById(R.id.listView);
+        ArrayList<Highscore> highScores = (ArrayList<Highscore>) IHighscorePref.getScore();
 
-        mAdapter = new MyAdapter(highScoresStringArray);
+
+        // HighscoreAdapter adapter = new HighscoreAdapter(this, R.layout.adapter_view_layout, highScores);
+        // myListView.setAdapter(adapter);
 
         return root;
     }
 }
 
-class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
-    private String[] mDataset;
 
-    // Provide a reference to the views for each data item
-    // Complex data items may need more than one view per item, and
-    // you provide access to all the views for a data item in a view holder
-    public static class MyViewHolder extends RecyclerView.ViewHolder {
-        // each data item is just a string in this case
-        public TextView textView;
-        public MyViewHolder(View v) {
-            super(v);
-            textView = v.findViewById(R.id.listView);
-        }
-    }
 
-    // Provide a suitable constructor (depends on the kind of dataset)
-    public MyAdapter(String[] myDataset) {
-        mDataset = myDataset;
-    }
 
-    // Create new views (invoked by the layout manager)
-    @Override
-    public MyAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        // create a new view
-        View v = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.highscore, parent, false);
-        MyViewHolder vh = new MyViewHolder(v);
-        return vh;
-    }
-
-    // Replace the contents of a view (invoked by the layout manager)
-    @Override
-    public void onBindViewHolder(MyViewHolder holder, int position) {
-        // - get element from your dataset at this position
-        // - replace the contents of the view with that element
-        holder.textView.setText(mDataset[position]);
-
-    }
-
-    // Return the size of your dataset (invoked by the layout manager)
-    @Override
-    public int getItemCount() {
-        return mDataset.length;
-    }
-}
-
-/*
-        IHighscorePref = new HighscorePrefManager(getActivity());
-        View root = inflater.inflate(R.layout.highscore, container, false);
-
-        sp = getActivity().getApplicationContext().getSharedPreferences("Winners", Context.MODE_PRIVATE);
-        int gamesCount = sp.getInt("gamesCount", 0);
-
-       highscoreArr = new String[gamesCount];
-
-        if(!(gamesCount == 0)){
-            for (int j = 0; j <gamesCount ;j++) {
-                String navn = sp.getString("navn_gamesCount" + j, "(Navn)");
-                int forkerte = sp.getInt("antalForkerte_gamesCount" + j, 0);
-                String orderet = sp.getString("orderet_gameCount" + j, "(orderet)");
-
-                highscoreArr[j] = navn + " havde " + forkerte + " forkerte ord" +"\n" + "Orderet var: " + orderet;
-
-            }
-        }
-
-        ArrayAdapter adapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, android.R.id.text1, highscoreArr);
-        listView = root.findViewById(R.id.listView);
-        listView.setAdapter(adapter);
-
-        return root;
-    }*/
